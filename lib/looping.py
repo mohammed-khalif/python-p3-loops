555a50c9671825478417611d97a0b542e5945e04
mohammed-khalif <mohamed.yusuf@student.moringaschool.com>
2023-05-16 12:32:53 -0400

    completed

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,34 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    counter = 10
+    while counter > 0:
+        print(counter)
+        counter -= 1
+    print("Happy New Year!")
 
-def square_integers(int_list):
-    # code goes here!
-    pass
+
+
+    
+        
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):
+       if num % 3 == 0 and num % 5 == 0:
+          print ("FizzBuzz")
+       elif num % 3 == 0:
+         print ("Fizz")
+       elif num % 5 == 0:
+         print ("Buzz")
+       else:
+         print (num)
+         
+
+def reverse_string(string):
+    reversed_string = ""
+    for char in string:
+        reversed_string = char + reversed_string
+    return reversed_string
+
+def square_integers(int_list):
+    return [num**2 for num in int_list]
